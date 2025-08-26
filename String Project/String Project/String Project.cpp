33962358f11974e48829d0607b81719149d9b2c4
Mohammed Basher <101946147+mohammedbasherly@users.noreply.github.com>
2025-08-26 16:17:20 +0200

    Add files via upload

---
+++ String Project/String Project/String Project.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+#include "C:\Programming\Libraries\clsString.h";
+
+#include "C:\Programming\Libraries\global_funcs.h";
+
+
+int main()
+{
+	clsString string1;
+	string1.value = "Mohammed Basher";
+
+	cout << "String 1:" << endl;
+	cout << "Value: " << string1.value << endl;
+	cout << "Number of Words in \"" << string1.value << "\" is: " << string1.countWords() << endl;
+	cout << "Number of Capital letters: " << string1.countCapitalLetters() << endl;
+	cout << "Number of small letters: " << string1.countSmallLetters() << endl;
+	cout << "Number of \'m\' character: " << string1.countCharacter('m', false) << endl;
+	cout << "First letter: " << string1.getFirstLetterOfWord() << endl;
+	cout << "First word: " << string1.getFirstWordFromText() << endl;
+	cout << "================================" << endl;
+	cout << endl;
+
+	string1.replaceWordInString("Mohammed Basher", "Basher Abdullatif -.");
+	cout << "String 2:" << endl;
+	cout << "Value: " << string1.value << endl;
+	string1.invertAllLettersCase();
+	string1.printEachWordSeperatly(" - ");
+	cout << endl;
+	string1.trim();
+	string1.removeAllPuncuations();
+	string1.invertAllLettersCase();
+	cout << "Value: " << string1.value << endl;
+
+
+	return 0;
+}
